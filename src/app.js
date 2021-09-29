f78a2eecbbf97504320f9b1d634cb55bcc9f47ec
areeb <sea.095e@gmail.com>
2021-09-29 13:03:18 +0500

    update

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.get("/post",(req,res)=>{
+app.get("/",(req,res)=>{
     res.json({
         "mes" : "hello",
         "and": "harami"
