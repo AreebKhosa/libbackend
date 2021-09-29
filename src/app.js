288982f8598d9ac4147e8a36a23aa548b4cee4ce
areeb <sea.095e@gmail.com>
2021-09-29 12:44:40 +0500

    aysy hi

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const createErrors = require("http-errors")
+const UserRotues = require("./routes/app.routes")
+require("./helpers/conn")
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/post",(req,res)=>{
+    res.json({
+        "mes" : "hello",
+        "and": "harami"
+    })
+})
+
+app.use("/user",UserRotues)
+
+app.use(async(req,res,next)=>{
+    await next(createErrors.NotFound())
+})
+
+// all error come here
+
+app.use((err,req,res,next) =>{
+    res.status(err.status || 500)
+    res.send({
+        error: {
+            status:err.status || 500,
+            message: err.message
+        }
+    })
+})
+
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT,()=>{
+    console.log("app is running on ",PORT)
+})
